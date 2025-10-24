4ce7b99a8f536549030979e45c85fcfa9ea9387a
Yasin Okunakol <yasinokunakol@gmail.com>
2025-10-24 19:43:56 +0200

    google anly

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -15,6 +15,13 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "select_account",   // 👈 her zaman hesap seçtirir
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
   callbacks: {
