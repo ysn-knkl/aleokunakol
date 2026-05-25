0e7a18c1351a410e52905fad67c72a671b81b360
Yasin Okunakol <yasinokunakol@gmail.com>
2026-05-25 22:46:46 +0200

    redeploy

--- pages/api/activecampaign/subscribe.ts
+++ pages/api/activecampaign/subscribe.ts
@@ -9,6 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(405).json({ message: "Method not allowed" });
     }
 
+
     const { email } = req.body;
 
     if (!email || typeof email !== "string") {
